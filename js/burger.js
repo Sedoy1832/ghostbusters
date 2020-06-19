81675537cba091479bc008c67c74f65a52c5a7db
Sedoy1832 <67161276+Sedoy1832@users.noreply.github.com>
2020-06-19 19:13:33 +0300

    Upload all files

---
+++ js/burger.js
@@ -0,0 +1,9 @@
+$(".m-line").click(function () {
+  if ($(this).is(".active:not(.back)")) {
+    $(this).addClass("back");
+  } else if ($(this).is(".back")) {
+    $(this).removeClass("back");
+  } else {
+    $(this).addClass("active");
+  }
+});
